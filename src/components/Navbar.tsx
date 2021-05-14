bb0a755c7370d8ca95dcce1e72dcd61b6ff15247
wavilikhin <vavilikhin.v@gmail.com>
2021-05-14 18:08:06 +0300

    Adds caclulation for underline element starting position

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 type City = {
   section: string;
@@ -14,6 +14,12 @@ export const Navbar: React.FC<{ cities?: City[] }> = ({ cities = [] }) => {
     width: 68,
   });
 
+  const middleLiElement = useRef<HTMLLIElement>(null);
+  const nextToMiddleLiElement = useRef<HTMLLIElement>(null);
+
+  const middleLiElementIndex = Math.floor((cities.length - 1) / 2);
+  const nextToMiddleLiElementIndex = middleLiElementIndex + 1;
+
   function changeActiveElement(e: React.MouseEvent<HTMLLIElement>, city: City) {
     changeActiveCity(city);
 
@@ -23,20 +29,46 @@ export const Navbar: React.FC<{ cities?: City[] }> = ({ cities = [] }) => {
     });
   }
 
+  useEffect(() => {
+    if (middleLiElement.current && nextToMiddleLiElement.current) {
+      const underlineStartingPoint =
+        middleLiElement.current.offsetLeft +
+        middleLiElement.current.offsetWidth;
+
+      const underlineStartingWidth =
+        (nextToMiddleLiElement.current?.offsetLeft - underlineStartingPoint) *
+        0.6;
+
+      changeUnderlinePosition({
+        left: underlineStartingPoint,
+        width: underlineStartingWidth,
+      });
+    }
+  }, []);
+
   return (
     <nav className='cities'>
       <ul className='cities-list'>
-        {cities.map((city) => (
-          <li
-            className={`cities-item ${
-              city.section === activeCity.section ? 'cities-item--active' : ''
-            }`}
-            onClick={(e) => changeActiveElement(e, city)}
-            key={city.section + city.label}
-          >
-            {city.label}
-          </li>
-        ))}
+        {cities.map((city, i) => {
+          return (
+            <li
+              className={`cities-item ${
+                city.section === activeCity.section ? 'cities-item--active' : ''
+              }`}
+              onClick={(e) => changeActiveElement(e, city)}
+              key={city.section + city.label}
+              ref={
+                i === middleLiElementIndex
+                  ? middleLiElement
+                  : i === nextToMiddleLiElementIndex
+                  ? nextToMiddleLiElement
+                  : null
+              }
+            >
+              {city.label}
+            </li>
+          );
+        })}
       </ul>
       <span
         style={{ left: underlinePosition.left, width: underlinePosition.width }}
